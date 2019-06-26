f6d1a0389e52ef866ad55d8ec3248a2e362439f5
alinabeglarian <alinabeglarian@gmail.com>
2019-06-26 16:34:21 +0200

    put

--- team/router.js
+++ team/router.js
@@ -21,4 +21,25 @@ router.post('/team', function (req, res, next) {
     })
 })
 
+router.get('/team/:id', function (req, res, next) {
+  const id = req.params.id
+  Team.findByPk(id)
+    .then(teams => {
+      res.json(teams)
+    })
+    .catch(err => {
+      next(err)
+    })
+})
+
+router.put('/team/:id', function (req, res, next) {
+  const id = req.params.id
+  Team.findByPk(id)
+    .then(team => team.update(req.body))
+    .then(team => res.json(team))
+    .catch(err => {
+      next(err)
+    })
+})
+
 module.exports = router
