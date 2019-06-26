159c1864a19e17ef8dfa8291f3d745bd3865f15c
alinabeglarian <alinabeglarian@gmail.com>
2019-06-26 14:48:14 +0200

    schema

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const sequelize = require('./db')
+const Team = require('./team/model')
 
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Listening on: ${port}`))
-
