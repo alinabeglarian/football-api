969fcd1e2aa5f5647bffcab8352a219b1843c22d
alinabeglarian <alinabeglarian@gmail.com>
2019-06-26 11:47:30 +0200

    minor change

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 
 const port = process.env.PORT || 4000
-app.listen(port, () => console.log(`Listening on :${port}`))
+app.listen(port, () => console.log(`Listening on: ${port}`))
