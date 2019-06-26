b8d93a3333094796afa5804dd958e4b1d663485c
alinabeglarian <alinabeglarian@gmail.com>
2019-06-26 14:07:34 +0200

    heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const sequelize = require('./db')
 
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Listening on: ${port}`))
+
